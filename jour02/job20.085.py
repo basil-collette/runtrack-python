8694cb0a8b0ae5bd4dd68b19947c976fbc851167
basil-collette <basil.collette@laplateforme.io>
2023-03-29 09:46:10 +0200

    improvment ai

--- jour02/job20.085.py
+++ jour02/job20.085.py
@@ -6,33 +6,33 @@ class LocType(Enum):
     RED = 'O'
     YELLOW = 'X'
     
-def should_win(board, row_pos, col_pos, player_color):
+def check_win(board, row_pos, col_pos, player_color):
     # Check horizontal
     for r in range(len(board)):
         for c in range(len(board[0]) - 3):
-            if board[r][c+1] == player_color and board[r][c+2] == player_color and board[r][c+3] == player_color:
+            if board[r][c] == player_color and board[r][c+1] == player_color and board[r][c+2] == player_color and board[r][c+3] == player_color:
                 return True
 
     # Check vertical
     for r in range(len(board) - 3):
         for c in range(len(board[0])):
-            if board[r+1][c] == player_color and board[r+2][c] == player_color and board[r+3][c] == player_color:
+            if board[r][c] == player_color and board[r+1][c] == player_color and board[r+2][c] == player_color and board[r+3][c] == player_color:
                 return True
 
     # Check diagonal (down-right)
     for r in range(len(board) - 3):
         for c in range(len(board[0]) - 3):
-            if board[r+1][c+1] == player_color and board[r+2][c+2] == player_color and board[r+3][c+3] == player_color:
+            if board[r][c] == player_color and board[r+1][c+1] == player_color and board[r+2][c+2] == player_color and board[r+3][c+3] == player_color:
                 return True
 
     # Check diagonal (up-right)
     for r in range(3, len(board)):
         for c in range(len(board[0]) - 3):
-            if board[r-1][c+1] == player_color and board[r-2][c+2] == player_color and board[r-3][c+3] == player_color:
+            if board[r][c] == player_color and board[r-1][c+1] == player_color and board[r-2][c+2] == player_color and board[r-3][c+3] == player_color:
                 return True
 
     return False
-   
+    
 def get_col_next_free_row_index(board, col_index):
     inserted = False
     row_index = -1
@@ -48,6 +48,12 @@ def get_col_next_free_row_index(board, col_index):
         
     return row_index
     
+def put_token(board, row_index, col_index, player_color):
+        if (board[row_index][col_index] == LocType.EMPTY.value):
+            board[row_index][col_index] = player_color
+        else:
+            raise Exception('position déjà occupée!')
+    
 class AI_One:
     def __init__(self, self_color):
         self.self_color = self_color
@@ -61,10 +67,14 @@ class AI_One:
                     count += 1
         return count
         
-    def get_best_col_index(self, board):
+    def get_best_col_index(self, board, locked_columns):
         col_index = [-1]
         col_score = 0
         for i in range(len(board[0])):
+            
+            if i in locked_columns:
+                continue
+            
             free_row_index_of_col_i = get_col_next_free_row_index(board, i)
             i_score = self.count_nearby_same_color(board, free_row_index_of_col_i, i)
             
@@ -86,8 +96,13 @@ class AI_One:
         
     def get_win_index_by_color(self, board, color):
         for i in range(len(board[0])):
-            free_row_index_of_col_i = get_col_next_free_row_index(board, i)
-            if (should_win(board, free_row_index_of_col_i, i, color) == True):
+            copy_board = [[i for i in row] for row in board]
+            
+            free_row_index_of_col_i = get_col_next_free_row_index(copy_board, i)
+            
+            put_token(copy_board, free_row_index_of_col_i, i, color)
+            
+            if (check_win(copy_board, free_row_index_of_col_i, i, color) == True):
                 return i
                 
         return -1
@@ -96,22 +111,33 @@ class AI_One:
         #check if has win possibility
         self_win_index = self.get_win_index_by_color(board, self.self_color.value)
         if (self_win_index != -1):
-            print("auto-win strategy")
             return self_win_index
         
         #return col of enemy future winning
         enemy_win_index = self.get_win_index_by_color(board, self.enemy_color.value)
         if (enemy_win_index != -1):
-            print("enemy-win strategy")
             return enemy_win_index
             
         #return col with the best amout of self_color nearby
-        best_col_index = self.get_best_col_index(board)
-        if(best_col_index != -1):
-            print("best col strategy")
-            return best_col_index
+        best_col_index = self.get_best_col_index(board, [])
         
-        print("random strategy")
-        #return random near the middle
-        return random.randint(2, len(board[0])-3)
+        if(best_col_index == -1):
+            #return random near the middle
+            return random.randint(2, len(board[0])-3)
+        
+        #repeat step untill best_col_index not permit player to win
+        locked_columns = []
+        unsafe = True
+        while (unsafe == True):
+            best_col_index = self.get_best_col_index(board, locked_columns)
+            copy_board = [[i for i in row] for row in board]
+            free_row_index_of_col_i = get_col_next_free_row_index(copy_board, best_col_index)
+            put_token(copy_board, free_row_index_of_col_i, best_col_index, self.self_color.value)
+            index = self.get_win_index_by_color(copy_board, self.enemy_color.value)
+            if (index != -1):
+                locked_columns.append(best_col_index)
+            else:
+                unsafe = False
+        
+        return best_col_index
 
